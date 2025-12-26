5f28926bfa3376e51d2697d88eca50a14bc4e024
100ratings <omentalistademian@gmail.com>
2025-12-26 11:58:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const FORCE_OFFSET_FROM_END = 7;
 const FORCE_EXTRA_DELAY = 200;
 
 const SPEED_START = 60;
-const SPEED_END = 38;
-const AFTER_FLUSH_DELAY = 18;
+const SPEED_END = 30;
+const AFTER_FLUSH_DELAY = 8;
 
 // ===============================
 // BARALHO — MNEMONICA ROTACIONADA
@@ -249,5 +249,6 @@ document.addEventListener("touchend", e => {
 deckEl.addEventListener("click", () => {
   if (!awaitingRetry) startDeck();
 });
+
 
 
